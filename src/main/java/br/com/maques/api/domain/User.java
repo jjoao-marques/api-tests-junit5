a3e690797459289574bf6836bc284cf2f5dfd726
jjoao-marques <joaomarques.jm991@gmail.com>
2022-01-05 23:42:07 -0300

    v1 - User Repository

--- src/main/java/br/com/maques/api/domain/User.java
+++ src/main/java/br/com/maques/api/domain/User.java
@@ -18,7 +18,7 @@ public class User {
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
     private String name;
 
