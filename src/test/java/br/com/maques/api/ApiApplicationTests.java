32957fa7241113b95cff9cbbb9e6f03fb2c4fc6d
jjoao-marques <joaomarques.jm991@gmail.com>
2022-01-05 22:01:52 -0300

    Projeto iniciado

---
+++ src/test/java/br/com/maques/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maques.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
