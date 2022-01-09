e25da59e63a432562b6ae1ae12b2f1dbf6ae7ad4
jjoao-marques <joaomarques.jm991@gmail.com>
2022-01-09 02:14:29 -0300

    Exception - Email already exists

--- src/main/java/br/com/maques/api/Utils/MessageUtil.java
+++ src/main/java/br/com/maques/api/Utils/MessageUtil.java
@@ -3,4 +3,5 @@ package br.com.maques.api.Utils;
 public abstract class MessageUtil {
 
     public static final String OBJECT_NOT_FOUND = "Objeto não encontrado!";
+    public static final String EMAIL_ALREADY_EXISTS = "E-mail já cadastrado na base de dados!";
 }
--- src/main/java/br/com/maques/api/services/Impl/UserServiceImpl.java
+++ src/main/java/br/com/maques/api/services/Impl/UserServiceImpl.java
@@ -5,12 +5,14 @@ import br.com.maques.api.domain.User;
 import br.com.maques.api.domain.dto.UserDTO;
 import br.com.maques.api.repositories.UserRepository;
 import br.com.maques.api.services.UserService;
+import br.com.maques.api.services.exceptions.DataIntegrityViolationException;
 import br.com.maques.api.services.exceptions.ObjectNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -32,6 +34,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User create(UserDTO obj) {
+        findByEmail(obj);
         return userRepository.save(mapper.map(obj, User.class));
+    }
+
+    private void findByEmail(UserDTO objDto) {
+        Optional<User>  user = userRepository.findByEmail(objDto.getEmail());
+        if(user.isPresent()) {
+            throw  new DataIntegrityViolationException(MessageUtil.EMAIL_ALREADY_EXISTS);
+        }
     }
 }
