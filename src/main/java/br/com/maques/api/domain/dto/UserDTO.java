40b8c22e96996b7ee906976640fae4e0d0d4b30c
jjoao-marques <joaomarques.jm991@gmail.com>
2022-01-09 00:57:11 -0300

    v1 - Class UserDTO

---
+++ src/main/java/br/com/maques/api/domain/dto/UserDTO.java
@@ -0,0 +1,19 @@
+package br.com.maques.api.domain.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+}
