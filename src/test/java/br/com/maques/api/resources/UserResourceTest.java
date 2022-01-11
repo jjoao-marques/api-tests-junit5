23bb3fe4f6ed08efe2818c07ef8dea360b7cdb93
jjoao-marques <joaomarques.jm991@gmail.com>
2022-01-10 23:38:01 -0300

    Teste para o endpoint findById da classe UserResource

---
+++ src/test/java/br/com/maques/api/resources/UserResourceTest.java
@@ -0,0 +1,84 @@
+package br.com.maques.api.resources;
+
+import br.com.maques.api.domain.User;
+import br.com.maques.api.domain.dto.UserDTO;
+import br.com.maques.api.services.Impl.UserServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest
+class UserResourceTest {
+
+    public static final Long ID = 1L;
+    public static final String NAME = "João";
+    public static final String EMAIL = "joao@mail.com";
+    public static final String PASSWORD = "123";
+
+
+    private User user;
+    private UserDTO userDTO;
+
+    @InjectMocks
+    private UserResource userResource;
+
+    @Mock
+    private UserServiceImpl userService;
+
+    @Mock
+    private ModelMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void whenFindByIdThenReturnSuccess() {
+
+        Mockito.when(userService.findById(Mockito.anyLong())).thenReturn(user);
+        Mockito.when(mapper.map(Mockito.any(), Mockito.any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = userResource.findById(ID);
+
+        Assertions.assertNotNull(response);
+        Assertions.assertNotNull(response.getBody());
+        Assertions.assertEquals(ResponseEntity.class, response.getClass());
+        Assertions.assertEquals(UserDTO.class, response.getBody().getClass());
+
+        Assertions.assertEquals(ID, response.getBody().getId());
+        Assertions.assertEquals(NAME, response.getBody().getName());
+        Assertions.assertEquals(EMAIL, response.getBody().getEmail());
+        Assertions.assertEquals(PASSWORD, response.getBody().getPassword());
+
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser() {
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+    }
+}
