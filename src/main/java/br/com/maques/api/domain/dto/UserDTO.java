c901232b35ce141f33b49bd2c151a750b3331f4b
jjoao-marques <joaomarques.jm991@gmail.com>
2022-01-12 00:31:51 -0300

    Removendo Bug

--- src/main/java/br/com/maques/api/domain/dto/UserDTO.java
+++ src/main/java/br/com/maques/api/domain/dto/UserDTO.java
@@ -4,8 +4,10 @@ package br.com.maques.api.domain.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
 @Getter @Setter
 @AllArgsConstructor
 public class UserDTO {
